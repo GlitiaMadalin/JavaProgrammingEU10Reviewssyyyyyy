eda02191e8d0408224ad2ad0d038cdeb93e12f64
AdamB25 <barsialtunit@gmail.com>
2022-12-17 19:43:17 +0300

    week 15 task 1 is done

--- src/week15_12_17_2022/method_overriding/task1/INGBank.java
+++ src/week15_12_17_2022/method_overriding/task1/INGBank.java
@@ -1,10 +1,9 @@
 package week15_12_17_2022.method_overriding.task1;
 
-public class GarantiBank extends Bank {
-
+public class INGBank extends Bank{
     @Override
     public void calculateTheInterest() {
         super.calculateTheInterest();
-        System.out.println(getCapital() * 8 / 100);
+        System.out.println(getCapital()*9/100);
     }
 }
