60e4859aa4effc4f591fccf7e54bac6983094d2b
AdamB25 <barsialtunit@gmail.com>
2023-01-07 20:43:56 +0300

    week 18 abstraction

---
+++ src/week_18_01_07_2023/abstraction/Ihpone.java
@@ -0,0 +1,28 @@
+package week_18_01_07_2023.abstraction;
+
+public class Ihpone extends Phone implements HasIOS,HasSiri,HasFaceTime{
+    @Override
+    public void hasFaceTime() {
+
+    }
+
+    @Override
+    public void hasIOS() {
+
+    }
+
+    @Override
+    public void hasSiri() {
+
+    }
+
+    @Override
+    void call() {
+
+    }
+
+    @Override
+    void text() {
+
+    }
+}
