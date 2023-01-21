fcf9ea618dc651a7cd27437b6355cc93d3e20775
AdamB25 <barsialtunit@gmail.com>
2023-01-21 22:45:43 +0300

    week 20 Task 2 is ready

---
+++ src/week20_01_21_2023/Task2.java
@@ -0,0 +1,20 @@
+package week20_01_21_2023;
+
+public class Task2 {
+      /*
+
+  --    create a list put the names
+         ahmet
+         rafet
+         mehmet
+         saffet
+         temel
+     -- create a method and return the sorted list
+      ahmet ,mehmet,rafet,saffet,temel
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+}
