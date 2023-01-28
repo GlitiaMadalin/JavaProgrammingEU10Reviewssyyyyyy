6c7659c3245a15368d4526b8c2ab9d10b9dc18e0
AdamB25 <barsialtunit@gmail.com>
2023-01-28 18:49:06 +0300

    week 21 Iterator example is done

--- src/wek21_01_28_2023/RemoveWordsWithIterator.java
+++ src/wek21_01_28_2023/RemoveWordsWithIterator.java
@@ -1,9 +1,28 @@
 package wek21_01_28_2023;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class RemoveWordsWithIterator {
 
     public static void main(String[] args) {
 
+        List<String> words=new ArrayList<>(Arrays.asList("apple","banana","cherry","Cydeo","dog"));
+
+        //I want to remove the word which is starting with a
+
+
+        Iterator<String> iterator = words.iterator();
+
+        while(iterator.hasNext()) {
+            String word = iterator.next();
+            if (word.startsWith("a")){
+                iterator.remove();
+            }
+        }
+        System.out.println(words);
 
 
     }
